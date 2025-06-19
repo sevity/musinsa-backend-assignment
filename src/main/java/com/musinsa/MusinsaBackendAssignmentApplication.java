2320fb16e667abb1b476afd998a0266c37ddb51c
Wonil Kim <linowmik@gmail.com>
2025-06-19 21:26:09 +0900

    initial commit

--- src/main/java/com/musinsa/MusinsaBackendAssignmentApplication.java
+++ src/main/java/com/musinsa/MusinsaBackendAssignmentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MusinsaBackendAssignmentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MusinsaBackendAssignmentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MusinsaBackendAssignmentApplication.class, args);
+    }
 
 }
--- src/test/java/com/musinsa/MusinsaBackendAssignmentApplicationTests.java
+++ src/test/java/com/musinsa/MusinsaBackendAssignmentApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MusinsaBackendAssignmentApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
