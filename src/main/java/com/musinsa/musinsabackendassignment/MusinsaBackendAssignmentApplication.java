a740b3437c2910f59efcebb5f49f426f3256a20c
Wonil Kim <linowmik@gmail.com>
2025-06-19 21:03:07 +0900

    initial commit

---
+++ src/main/java/com/musinsa/musinsabackendassignment/MusinsaBackendAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.musinsabackendassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaBackendAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusinsaBackendAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musinsa/musinsabackendassignment/MusinsaBackendAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.musinsabackendassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaBackendAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
