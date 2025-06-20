3a8bbfdb6f4f6fcfd421752b713a06545478ac72
Wonil Kim <linowmik@gmail.com>
2025-06-21 01:03:35 +0900

    fix error

--- src/main/java/com/musinsa/domain/Brand.java
+++ src/main/java/com/musinsa/domain/Brand.java
@@ -1,24 +1,32 @@
-// domain/Brand.java
+// src/main/java/com/musinsa/domain/Brand.java
 package com.musinsa.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Getter @Setter @NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Brand {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, nullable = false)
     private String name;
 
     @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Product> products;
+    private List<Product> products = new ArrayList<>();
 
-    public Brand(String name) { this.name = name; }
+    public Brand(String name) {
+        this.name = name;
+    }
 }
